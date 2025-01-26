c54151be015ab934ebf95e7935827843dc1dfbab
ml3424 <ml3424@bs.amalnet.k12.il>
2025-01-26 23:02:24 +0200

    added credit screen

---
+++ app/src/main/java/com/example/ex0921_alert_dialog/CreditsActivity.java
@@ -0,0 +1,20 @@
+package com.example.ex0921_alert_dialog;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+
+
+    }
+}
